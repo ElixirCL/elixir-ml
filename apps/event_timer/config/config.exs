795183a28ddc5d4579c09badc172581f7e397a02
TFariasMath <tfarias3@alumno.uned.es>
2026-04-12 23:55:00 -0400

    test(event_timer): corregir eaddrinuse desactivando servidor web en test

--- apps/event_timer/config/config.exs
+++ apps/event_timer/config/config.exs
@@ -2,14 +2,13 @@ import Config
 
 config :event_timer, EventTimerWeb.Endpoint,
   url: [host: "localhost"],
-  http: [ip: {127, 0, 0, 1}, port: 4000],
-  server: true,
+  http: [ip: {127, 0, 0, 1}, port: if(Mix.env() == :test, do: 4002, else: 4000)],
+  server: Mix.env() != :test,
   adapter: Bandit.PhoenixAdapter,
   live_view: [signing_salt: "event_timer_secret_salt_12345"]
 
 config :event_timer, :app_path,
-  windows:
-    "C:\\Users\\USER\\Documents\\Apuntes_Elixir_Libro\\elixir-ml\\event_timer\\_build\\prod\\rel\\event_timer\\bin\\event_timer.bat"
+  windows: "event_timer.bat"
 
 config :logger, :console,
   format: "$time $level $message\n",
